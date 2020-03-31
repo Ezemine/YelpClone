eb290dc60bb6f9b1c828e0045959afd112a65d5b
Ezemine <32526879+Ezemine@users.noreply.github.com>
2020-03-30 20:19:03 -0500

    Added steps to make it launch from github

---
+++ build/precache-manifest.31f04fe53bd4d3c6dcc0137a8d5eba8b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af0e0626670385c737606fc596b16463",
+    "url": "/YelpClone/index.html"
+  },
+  {
+    "revision": "5441e3219b08de3b2977",
+    "url": "/YelpClone/static/css/main.cf9d54d7.chunk.css"
+  },
+  {
+    "revision": "a6ac2e83ec7c797a0bbf",
+    "url": "/YelpClone/static/js/2.95616783.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/YelpClone/static/js/2.95616783.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5441e3219b08de3b2977",
+    "url": "/YelpClone/static/js/main.af02b852.chunk.js"
+  },
+  {
+    "revision": "ce27e495bc135a157f3f",
+    "url": "/YelpClone/static/js/runtime-main.ff9f4933.js"
+  },
+  {
+    "revision": "75c1df99224e2321a1680033e0aac7fb",
+    "url": "/YelpClone/static/media/large_background.75c1df99.jpg"
+  },
+  {
+    "revision": "dcdb1b62bfa762edd7223d532b225980",
+    "url": "/YelpClone/static/media/small_background.dcdb1b62.jpg"
+  }
+]);
